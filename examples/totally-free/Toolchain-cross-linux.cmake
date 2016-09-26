2f92d79f5de84d73b0f02f9bc8cec20c025aa1d1
Eric NOULARD <eric.noulard@gmail.com>
2016-09-26 18:18:11 +0200

    Update CPack part and bibliography

--- examples/totally-free/Toolchain-cross-linux.cmake
+++ examples/totally-free/Toolchain-cross-linux.cmake
@@ -2,11 +2,9 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # Choose an appropriate compiler prefix
-
 # for classical mingw32
 # see http://www.mingw.org/
 #set(COMPILER_PREFIX "i586-mingw32msvc")
-
 # for 32 or 64 bits mingw-w64
 # see http://mingw-w64.sourceforge.net/
 set(COMPILER_PREFIX "i686-w64-mingw32")
@@ -20,11 +18,9 @@ find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
 find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
 #SET(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
 
-
 # here is the target environment located
 SET(USER_ROOT_PATH /home/erk/erk-win32-dev)
 SET(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX} ${USER_ROOT_PATH})
-
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
